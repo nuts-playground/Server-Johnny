599922e8f2c36984a1e83a8387044c706e28b766
devssk <sksong419@naver.com>
2023-12-01 13:48:51 +0900

    feat: 여신협회 스크래핑 기능 추가중, 로그인 및 세션 가져오는 기능 추가

---
+++ src/main/java/com/backend/johnny/controller/CfaController.java
@@ -0,0 +1,19 @@
+package com.backend.johnny.controller;
+
+import com.backend.johnny.service.CfaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cfa")
+public class CfaController {
+
+    private CfaService cfaService;
+
+    @GetMapping("/cookie")
+    public String getCookie(@RequestParam(name = "userId") String userId, @RequestParam(name = "userPw") String userPw) {
+        return cfaService.getCookie(userId, userPw);
+    }
+
+}
---
+++ src/main/java/com/backend/johnny/service/CfaService.java
@@ -0,0 +1,59 @@
+package com.backend.johnny.service;
+
+import com.backend.johnny.dto.cfa.CfaRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CfaService {
+
+    private RestTemplate restTemplate;
+
+    public String getCookie(String userId, String userPw) {
+        String url = "https://www.cardsales.or.kr/authentication";
+        String cookie = null;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("j_username", userId);
+        body.add("j_password", userPw);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                request,
+                String.class
+        );
+
+        HttpHeaders responseHeaders = response.getHeaders();
+        List<String> cookieList = responseHeaders.get("Set-Cookie");
+        if (cookieList != null && !cookieList.isEmpty()) {
+            String[] split = cookieList.get(0).split(";");
+
+            for (String part : split) {
+                part = part.trim();
+                if (part.startsWith("JSESSIONID=")) {
+                    cookie += part;
+                    break;
+                }
+            }
+        }
+
+        return cookie;
+    }
+
+}
