5247b142df2eb0b34b0ad47c48e6debaa230155b
devssk <sksong419@naver.com>
2023-12-18 15:50:20 +0900

    fix: ApiIntercepter IP주소 출력 수정

--- src/main/java/com/backend/johnny/intercepter/ApiIntercepter.java
+++ src/main/java/com/backend/johnny/intercepter/ApiIntercepter.java
@@ -13,35 +13,14 @@ public class ApiIntercepter implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         request.setAttribute("api-time", System.currentTimeMillis());
-        String ip = request.getHeader("X-FORWARDED-FOR");
-        log.info("TEST : X-FORWARDED-FOR : " + ip);
-        if (ip == null) {
-            ip = request.getHeader("Proxy-Client-IP");
-            log.info("TEST : Proxy-Client-IP : " + ip);}
-        if (ip == null) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-            log.info("TEST : WL-Proxy-Client-IP : " + ip);
-        }
-        if (ip == null) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-            log.info("TEST : HTTP_CLIENT_IP : " + ip);
-        }
-        if (ip == null) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            log.info("TEST : HTTP_X_FORWARDED_FOR : " + ip);
-        }
-        if (ip == null) {
-            ip = request.getRemoteAddr();
-            log.info("TEST : RemoteAddr : " + ip);
-        }
-        log.info("request uri : " + request.getRequestURI() + ", remoteAddr : " + request.getRemoteAddr());
+        log.info("request uri : " + request.getRequestURI() + ", IP : " + request.getHeader("X-FORWARDED-FOR"));
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         long time = (long) request.getAttribute("api-time");
-        log.info("request uri : " + request.getRequestURI() + ", remoteAddr : " + request.getRemoteAddr() + ", response status : " + response.getStatus() + ", total time : " + (System.currentTimeMillis() - time) + "ms");
+        log.info("request uri : " + request.getRequestURI() + ", IP : " + request.getHeader("X-FORWARDED-FOR") + ", response status : " + response.getStatus() + ", total time : " + (System.currentTimeMillis() - time) + "ms");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
